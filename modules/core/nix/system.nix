99f922f603222a65075c0c7373762c2a577e7076
Aksel O. Steen <aksel@akselos.no>
2026-08-13 00:18:41 +0200

    fix: make stateVersion static

--- modules/core/nix/system.nix
+++ modules/core/nix/system.nix
@@ -1,8 +1,8 @@
-{ self, config, ... }:
+{ self, ... }:
 
 {
   system = {
-    stateVersion = config.system.nixos.release;
+    stateVersion = "26.05";
     configurationRevision = self.shortRev or self.dirtyShortRev or "dirty";
   };
 }
