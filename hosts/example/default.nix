5edc3f2a365c790ae4135524f84a16a7edd5a4cb
Aksel O. Steen <aksel@akselos.no>
2026-08-12 16:50:50 +0200

    feat: flesh out example hardware config

--- hosts/example/default.nix
+++ hosts/example/default.nix
@@ -5,4 +5,20 @@
     inputs.disko.nixosModules.disko
     ./disko.nix
   ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "example";
+  networking.useDHCP = true;
+
+  boot.initrd.availableKernelModules = [
+    "ahci"
+    "xhci_pci"
+    "virtio_pci"
+    "sr_mod"
+    "virtio_blk"
+  ];
+
+  users.users.root.initialPassword = "password";
 }
