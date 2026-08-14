f7ec3f6056946f1d10801559a84a3cbaa62ba2cd
Aksel O. Steen <aksel@akselos.no>
2026-08-15 00:11:21 +0200

    fix: add temporary defaultSopsFile

--- hosts/testbox/secrets.nix
+++ hosts/testbox/secrets.nix
@@ -1,9 +1,11 @@
-{ inputs, ... }:
+{ inputs, self, ... }:
 
 {
   imports = [
     inputs.sops-nix.nixosModules.sops
   ];
+
+  sops.defaultSopsFile = "${self}/secrets/services/wireguard.yaml";
 
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
