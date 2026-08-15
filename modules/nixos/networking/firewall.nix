ddb2eb0b87b1424e2ded410a8a33928d317834c9
Aksel O. Steen <aksel@akselos.no>
2026-08-15 23:02:08 +0200

    feat: open ports in firewall for containers

--- modules/nixos/networking/firewall.nix
+++ modules/nixos/networking/firewall.nix
@@ -4,7 +4,15 @@
       enable = true;
 
       # Defined in each service's config
-      allowedTCPPorts = [ ];
+      allowedTCPPorts = [
+        8080
+        7476
+        3000
+        3100
+        9090
+        9100
+        12345
+      ];
       allowedTCPPortRanges = [ ];
     };
 
