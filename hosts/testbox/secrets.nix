a29c3a3494eb92ce45bce47772bffea9a156bf9c
Aksel O. Steen <aksel@akselos.no>
2026-08-15 14:56:37 +0200

    feat: attempt better secrets formatting

--- hosts/testbox/secrets.nix
+++ hosts/testbox/secrets.nix
@@ -1,14 +1,24 @@
-{ inputs, self, ... }:
+{
+  inputs,
+  self,
+  config,
+  lib,
+  ...
+}:
 
+let
+  inherit (lib) genAttrs;
+in
 {
   imports = [
     inputs.sops-nix.nixosModules.sops
   ];
 
-  sops.defaultSopsFile = "${self}/secrets/services/wireguard.yaml";
+  sops.defaultSopsFile = "${self}/secrets/${config.networking.hostName}";
 
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
-  sops.secrets."key" = { };
-  sops.secrets."addr" = { };
+  sops.secrets = genAttrs [ "wireguard/key" "wireguard/address" ] {
+    sopsFile = "${self}/secrets/services/wireguard.yaml";
+  };
 }
