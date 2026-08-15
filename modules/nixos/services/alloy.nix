e5e53ebf28c142a3d4ae21b242cd78f115aaeb0e
Aksel O. Steen <aksel@akselos.no>
2026-08-16 01:54:57 +0200

    feat: move alloy to nix
    Now listens to rootful sock

---
+++ modules/nixos/services/alloy.nix
@@ -0,0 +1,52 @@
+{
+  services.alloy = {
+    enable = true;
+  };
+
+  users.groups.alloy = { };
+
+  users.users.alloy = {
+    isSystemUser = true;
+    group = "alloy";
+    extraGroups = [ "podman" ];
+  };
+
+  environment.etc."alloy/config.alloy" = {
+    text = ''
+      // Discover running Podman containers through Podman's Docker-compatible API.
+      discovery.docker "containers" {
+          host = "unix:///run/podman/podman.sock"
+      }
+
+
+      // Tail logs from discovered containers and forward them to Loki.
+      loki.source.docker "containers" {
+          host       = "unix:///run/podman/podman.sock"
+          targets    = discovery.docker.containers.targets
+          forward_to = [loki.write.default.receiver]
+      }
+
+      // Tail the host's systemd journal.
+      loki.source.journal "journal" {
+          max_age       = "12h"
+          relabel_rules = loki.relabel.journal.rules
+          forward_to    = [loki.write.default.receiver]
+      }
+                
+      loki.relabel "journal" {
+          forward_to = []
+
+          rule {
+              source_labels = ["__journal__systemd_unit"]
+              target_label  = "unit"
+          }
+      }
+
+      loki.write "default" {
+          endpoint {
+              url = "http://127.0.0.1:3100/loki/api/v1/push"
+          }
+      }
+    '';
+  };
+}
