179e6cc5c1e439474ba2b8b0be475fd3a6aa6a17
Aksel O. Steen <aksel@akselos.no>
2026-08-16 23:52:02 +0200

    feat: add abs and jellyfin

---
+++ modules/nixos/quadlets/audiobookshelf.nix
@@ -0,0 +1,21 @@
+{
+  virtualisation.quadlet.containers.audiobookshelf = {
+    containerConfig = {
+      image = "ghcr.io/dvplyr/audiobookshelf:latest";
+      publishPorts = [ "13378:80" ];
+      environments = {
+        TZ = "Europe/Oslo";
+      };
+
+      volumes = [
+        "/var/lib/audiobookshelf/config:/config"
+        "/var/lib/audiobookshelf/metadata:/metadata"
+      ];
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/audiobookshelf/config 0755 root root -"
+    "d /var/lib/audiobookshelf/metadata 0755 root root -"
+  ];
+}
---
+++ modules/nixos/quadlets/jellyfin.nix
@@ -0,0 +1,17 @@
+{
+  virtualisation.quadlet.containers.jellyfin = {
+    containerConfig = {
+      image = "docker.io/jellyfin/jellyfin:latest";
+      publishPorts = [ "8096:8096" ];
+      volumes = [
+        "/var/lib/jellyfin/config:/config"
+        "/var/lib/jellyfin/cache:/cache"
+      ];
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/jellyfin/config 0755 root root -"
+    "d /var/lib/jellyfin/cache 0755 root root -"
+  ];
+}
