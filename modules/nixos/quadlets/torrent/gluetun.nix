bbc0871eb42d66dacd550f73e952dd7a3e7aad8e
Aksel O. Steen <aksel@akselos.no>
2026-08-16 19:32:35 +0200

    feat: test podman secrets

--- modules/nixos/quadlets/torrent/gluetun.nix
+++ modules/nixos/quadlets/torrent/gluetun.nix
@@ -15,7 +15,11 @@ in
       addCapabilities = [ "NET_ADMIN" ];
       devices = [ "/dev/net/tun:/dev/net/tun" ];
 
-      volumes = [ "/var/lib/gluetun/config:/gluetun" ];
+      volumes = [
+        "/var/lib/gluetun/config:/gluetun"
+        "${secrets."wireguard/key".path}:/run/secrets/wireguard/key:ro"
+        "${secrets."wireguard/address".path}:/run/secrets/wireguard/address:ro"
+      ];
 
       healthCmd = "wget -qO- https://am.i.mullvad.net/connected";
       healthInterval = "30s";
@@ -24,10 +28,12 @@ in
 
       environments = {
         TZ = "Europe/Oslo"; # Change to target time.timeZone
+
         VPN_SERVICE_PROVIDER = "mullvad";
         VPN_TYPE = "wireguard";
-        WIREGUARD_PRIVATE_KEY_SECRETFILE = secrets."wireguard/key".path;
-        WIREGUARD_ADDRESSES_SECRETFILE = secrets."wireguard/address".path;
+
+        WIREGUARD_PRIVATE_KEY_SECRETFILE = "/run/secrets/wireguard/key";
+        WIREGUARD_ADDRESSES_SECRETFILE = "/run/secrets/wireguard/address";
       };
     };
   };
