874b159ed5c208ee841a28c1b52b55208014cc36
Aksel O. Steen <aksel@akselos.no>
2026-08-20 16:29:10 +0200

    feat: add lazylibrarian

---
+++ modules/nixos/quadlets/arr/lazylibrarian.nix
@@ -0,0 +1,30 @@
+{ config, ... }:
+
+let
+  inherit (config.virtualisation.quadlet) networks;
+
+in
+{
+  virtualisation.quadlet.containers.prowlarr = {
+    containerConfig = {
+      image = "docker.io/linuxserver/lazylibrarian:latest";
+      networks = [ networks.arr.ref ];
+      publishPorts = [ "5299:5299" ];
+
+      environments = {
+        TZ = "Europe/Oslo";
+        PUID = "1000";
+        PGID = "1000";
+      };
+
+      volumes = [
+        "/var/lib/lazylibrarian/config:/config"
+        "/data/torrents:/data/torrents"
+      ];
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/lazylibrarian/config 0755 1000 1000 -"
+  ];
+}
