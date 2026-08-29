675ecf7fe2bf63cedde13b966d24917a6da59ada
Aksel O. Steen <aksel@akselos.no>
2026-08-29 13:21:39 +0200

    feat(arr): explicit dns servers

--- modules/nixos/quadlets/arr/default.nix
+++ modules/nixos/quadlets/arr/default.nix
@@ -10,7 +10,17 @@
   ];
 
   virtualisation.quadlet.networks.arr = {
-    networkConfig.interfaceName = "dns-arr";
+    networkConfig = {
+      interfaceName = "dns-arr";
+      dns = [
+        "1.1.1.1" # Cloudflare
+        "1.0.0.1"
+        "9.9.9.9" # Quad9
+        "149.112.112.112"
+        "8.8.8.8" # Google (keeps logs)
+        "8.8.4.4"
+      ];
+    };
   };
 
   networking.firewall.interfaces."dns-arr".allowedUDPPorts = [ 53 ];
