4cd8dba1edba6ff45bad12b8608564f93a3a7444
Aksel O. Steen <aksel@akselos.no>
2026-09-06 16:43:51 +0200

    feat: add autobrr to traefik

--- modules/nixos/quadlets/arr/autobrr.nix
+++ modules/nixos/quadlets/arr/autobrr.nix
@@ -7,19 +7,18 @@ in
 {
   virtualisation.quadlet.containers.autobrr = {
     containerConfig = {
-      # labels = [
-      #   "traefik.enable=true"
-      #   "traefik.http.routers.autobrr.rule=Host(`autobrr.internal.akselos.no`)"
-      #   "traefik.http.routers.autobrr.entrypoints=websecure"
-      #   "traefik.http.routers.autobrr.tls=true"
-      #   "traefik.http.routers.autobrr.tls.certresolver=letsencrypt"
-      #   "traefik.http.services.autobrr.loadbalancer.server.port=7474"
-      #   "traefik.http.routers.autobrr.middlewares=purescale@file"
-      # ];
+      labels = [
+        "traefik.enable=true"
+        "traefik.http.routers.autobrr.rule=Host(`autobrr.internal.akselos.no`)"
+        "traefik.http.routers.autobrr.entrypoints=websecure"
+        "traefik.http.routers.autobrr.tls=true"
+        "traefik.http.routers.autobrr.tls.certresolver=letsencrypt"
+        "traefik.http.services.autobrr.loadbalancer.server.port=7474"
+        "traefik.http.routers.autobrr.middlewares=purescale@file"
+      ];
 
       image = "ghcr.io/autobrr/autobrr:latest";
       networks = [ networks.arr.ref ];
-      publishPorts = [ "100.101.183.74:7474:7474" ];
 
       environments = {
         TZ = config.time.timeZone;
