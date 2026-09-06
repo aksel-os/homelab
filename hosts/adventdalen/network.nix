c52013a1b238eeb8008404f3ea6eea219bd39e64
Aksel O. Steen <aksel@akselos.no>
2026-09-06 21:48:28 +0200

    fix: use wlp

--- hosts/adventdalen/network.nix
+++ hosts/adventdalen/network.nix
@@ -3,12 +3,12 @@
     "adventdalen-lan" = {
       connection = {
         id = "adventdalen-lan";
-        type = "ethernet";
-        interface-name = "enp4s0";
+        type = "wifi";
+        interface-name = "wlp5s0";
       };
       ipv4 = {
         method = "manual";
-        address1 = "192.168.10.10/24";
+        address1 = "192.168.0.100/24";
         gateway = "192.168.0.1";
         dns = "192.168.0.1;";
       };
